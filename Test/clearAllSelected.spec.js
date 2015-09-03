3a0328e4ac47d28528550106c116dad92485a5e1
Lukas Wilczek <lukas.wilczek@tillvaxtverket.se>
2015-09-03 21:01:12 +0200

    test fix

--- Test/clearAllSelected.spec.js
+++ Test/clearAllSelected.spec.js
@@ -6,6 +6,10 @@ describe('Stavros', function() {
   });
 
   it('should clear all selected', function() {
+    var activity1 = element(by.repeater('activity in activities').row(0));
+    activity1.element(by.className('activity-btn')).click();
+
     element(by.buttonText('Rensa alla valda')).click();
+
   }); 
 });
