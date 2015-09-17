73434d791b1b66485f40e68afbdfc5d7cc198043
Joel Binnquist <joel.binnquist@gmail.com>
2015-09-17 19:30:23 +0200

    Lägg till facit.

---
+++ test/facit/aktiviteter.spec.js
@@ -0,0 +1,29 @@
+'use strict';
+
+describe('Det ska finnas aktiviteter för användaren att utföra AWFK', function () {
+
+  it('Användaren ska kunna bocka för aktiviteter', function () {
+
+  });
+
+  it('Det ska gå att avbocka aktiviteter som dommarna dömmer bort', function () {
+
+  });
+
+  it('Aktiviteterna ska vara poängsatta', function () {
+
+  });
+
+  it('Aktiviteterna ska ha ett namn', function () {
+
+  });
+
+  it('Aktiviteterna ska ha en passande bild kopplad till sig', function () {
+
+  });
+
+  it('Bilderna till aktiviteterna ska gå att förstora', function () {
+
+  });
+
+});
