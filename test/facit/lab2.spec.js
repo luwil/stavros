1b0de636a2ce45423155cf397aa6d2f48ac2b3db
Anders Englesson <anders.englesson@cag.se>
2015-09-18 18:53:06 +0200

    lab 2 fixad

--- test/facit/lab2.spec.js
+++ test/facit/lab2.spec.js
@@ -2,9 +2,23 @@
 describe('Lab 2', function () {
   it('Aktiviteterna ska vara poängsatta, testa genom att bocka för alla aktiviteter och kontrollera att totalpoängen' +
     ' blir 86.', function () {
+    element.all(by.repeater('activity in activities'))
+      .each(function (rowElement) {
+        rowElement.element(by.className('activity-btn')).click();
+      });
+    expect(element(by.binding('getScore()')).getText()).toBe('86');
   });
 
   it('Det ska gå att logga in med namn. När man loggar in som "Sune" kommer namnet för' +
     ' LEVEL 1 bli "Sune Banan" och dessutom visas logout-länk.', function () {
+    expect(element(by.id('logout-link')).isPresent()).toBe(false);
+
+    element(by.model('userName')).sendKeys('Sune');
+    element(by.id('login-button')).click();
+
+    expect(element(by.binding('getLevel().name')).getText()).toBe('Sune Banan');
+    expect(element(by.id('logout-link')).isPresent()).toBe(true);
+
+    element(by.id('logout-link')).click();
   });
 });
