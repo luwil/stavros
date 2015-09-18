ef7f0ed42985fce0dd55928ebb911fea1aa20ca2
Joel Binnquist <joel.binnquist@gmail.com>
2015-09-18 19:23:54 +0300

    Lab1 och 2

--- test/facit/inloggning.spec.js
+++ test/facit/inloggning.spec.js
@@ -1,18 +1,18 @@
-'use strict'
+'use strict';
 
 describe('Användare ska kunna återkomma till appen', function () {
   beforeEach(function () {
     element(by.css('button[ng-click="clearSelected()"]')).click();
   });
 
-  it('Det ska gå att logga in med namn. När man loggar in som "Guldrun" kommer namnet för' +
-    ' level 1 bli "Guldrun Banan" och dessutom visas logout-länk.', function () {
+  it('Det ska gå att logga in med namn. När man loggar in som "Sune" kommer namnet för' +
+    ' level 1 bli "Sune Banan" och dessutom visas logout-länk.', function () {
     expect(element(by.id('logout-link')).isPresent()).toBe(false);
 
-    element(by.model('userName')).sendKeys('Guldrun');
+    element(by.model('userName')).sendKeys('Sune');
     element(by.id('login-button')).click();
 
-    expect(element(by.binding('getLevel().name')).getText()).toBe('Guldrun Banan');
+    expect(element(by.binding('getLevel().name')).getText()).toBe('Sune Banan');
     expect(element(by.id('logout-link')).isPresent()).toBe(true);
 
     element(by.id('logout-link')).click();
@@ -21,7 +21,7 @@ describe('Användare ska kunna återkomma till appen', function () {
   it('Det ska gå att logga ut. När man loggar ut försvinner logout-länk och level 1 namnet återgår till standard' +
     ' ("Sven Banan")', function () {
 
-    element(by.model('userName')).sendKeys('Guldrun');
+    element(by.model('userName')).sendKeys('Sune');
     element(by.id('login-button')).click();
 
 
