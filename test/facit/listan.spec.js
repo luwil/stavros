6941962b650eadefba8981a3c5ede9f6aa4d4bdb
Anders Englesson <anders.englesson@cag.se>
2015-09-18 18:29:28 +0200

    listan klar

---
+++ test/facit/listan.spec.js
@@ -0,0 +1,35 @@
+'use strict'
+
+describe('Det ska finnas en lista med grekiska aktiviteter', function (){
+
+    it('Det ska finnas minst 1 aktivitet i listan',function(){
+        expect(element.all(by.repeater('activity in activities')).count()).toBeGreaterThan(0);
+    });
+
+    it('Det ska finnas max 31 aktiviteter', function(){
+
+        var rowCount = 0;
+        element.all(by.repeater('activity in activities')).each(function(elements, index){
+            rowCount++;
+        }).then(function(){
+            expect(rowCount).toBe(31);
+        })
+
+
+    });
+
+    it('Det ska gå att filtrera aktiviteterna i listan', function(){
+
+        //Filtrering i fritextfält.
+        //Endast rätt rubriker eller poäng ska finnas med
+        //Det ska visas att inga resultat har hittats
+        element(by.css('body')).allowAnimations(false);
+        element(by.model('search')).sendKeys('DrIck').then(function(){
+            element.all(by.repeater('activity in activities')).each(function(elm, index){
+                expect(elm.getText()).toMatch(/drick/i);
+
+            });
+        });
+    });
+
+})
--- test/labbar/listan.spec.js
+++ test/labbar/listan.spec.js
@@ -3,28 +3,16 @@
 describe('Det ska finnas en lista med grekiska aktiviteter', function (){
 
     it('Det ska finnas minst 1 aktivitet i listan',function(){
-        element.all(by.repeater('activity in activities')).then(function(elements){
-            expect(elements[0].isPresent()).toBe(true);
-        })
+
     });
 
     it('Det ska finnas max 31 aktiviteter', function(){
 
-        var rowCount = 0;
-        element.all(by.repeater('activity in activities')).each(function(elements, index){
-            rowCount++;
-        }).then(function(){
-            expect(rowCount).toBe(31);
-        })
-
-
     });
 
     it('Det ska gå att filtrera aktiviteterna i listan', function(){
-        console.log('Det ska gå att filtrera aktiviteterna i listan')
-        //Filtrering i fritextfält.
-        //Endast rätt rubriker eller poäng ska finnas med
-        //Det ska visas att inga resultat har hittats
+
+
     });
 
 })
